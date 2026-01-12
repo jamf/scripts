45e369b956a3d57d38d9656e47c42139562b5c99
Graham Pugh <graham.pugh@jamf.com>
2026-01-12 18:14:01 +0100

    more suggestions based on shellcheck

--- services/Framework-Fixer/Framework-Fixer.sh
+++ services/Framework-Fixer/Framework-Fixer.sh
@@ -77,17 +77,17 @@ check_response() {
 
     if [[ ${http_code} =~ ^(200|201|202|204)$ ]]
     then
-        echo "$(date '+%Y-%m-%d %H:%M:%S') INFO: Command successfully sent: HTTP code: ${http_code}" >> ""${log_file}""
+        echo "$(date '+%Y-%m-%d %H:%M:%S') INFO: Command successfully sent: HTTP code: ${http_code}" >> "${log_file}"
     else
-        echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR: Command failed with HTTP error ${http_code}" >> ""${log_file}""
+        echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR: Command failed with HTTP error ${http_code}" >> "${log_file}"
         case ${http_code} in
-            400) echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR: Bad Request" >> ""${log_file}"" ;;
-            401) echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR: Unauthorized – check your credentials." >> ""${log_file}"" ;;
-            403) echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR: Forbidden – Insufficient privileges." >> ""${log_file}"" ;;
-            404) echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR: Not Found – resource does not exist." >> ""${log_file}"" ;;
-            409) echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR: Conflict – resource may already exist." >> ""${log_file}"" ;;
-            500) echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR: Server Error – try again later." >> ""${log_file}"" ;;
-            *)   echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR: Unexpected HTTP response: ${http_code}" >> ""${log_file}"" ;;
+            400) echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR: Bad Request" >> "${log_file}" ;;
+            401) echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR: Unauthorized - check your credentials." >> "${log_file}" ;;
+            403) echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR: Forbidden - Insufficient privileges." >> "${log_file}" ;;
+            404) echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR: Not Found - resource does not exist." >> "${log_file}" ;;
+            409) echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR: Conflict - resource may already exist." >> "${log_file}" ;;
+            500) echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR: Server Error - try again later." >> "${log_file}" ;;
+            *)   echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR: Unexpected HTTP response: ${http_code}" >> "${log_file}" ;;
         esac
     fi
     echo "${http_code}"
@@ -100,14 +100,15 @@ check_response() {
 auth_method_prompt() {
     echo "$(date '+%Y-%m-%d %H:%M:%S') INFO: Asking to select an authentication method" >> "${log_file}"
 
-    auth_method=$(
+    if ! auth_method=$(
         "${dialog_path}" \
             --title "Framework Fixer" \
             --message 'Welcome!
 
             Please select a method to authenticate to Jamf Pro. Click on "Required Privileges" for more information.' \
             --radio \
-            --selecttitle "Please select an option",radio --selectvalues "User Account & Password, API Client & Secret" \
+            --selecttitle "Please select an option",radio \
+            --selectvalues "User Account & Password, API Client & Secret" \
             --icon "${icon}" \
             --alignment "left" \
             --button2 \
@@ -117,7 +118,6 @@ auth_method_prompt() {
             --infobuttonaction "https://github.com/Sdelsaz/Framework-Fixer?tab=readme-ov-file#requirements" \
             --small
     )
-    if [[ $? -ne 0 ]]
     then
         echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING: User Cancelled" >> "${log_file}"
         invalidate_token
@@ -141,13 +141,13 @@ auth_method_prompt() {
 # Prompt for User Account and Password
 credential_prompt() {
     echo "$(date '+%Y-%m-%d %H:%M:%S') INFO: Prompting for credentials and server URL" >> "${log_file}"
-    server_details=$(
+    if ! server_details=$(
         "${dialog_path}" \
             --title "Framework Fixer" \
             --message "Please enter your Jamf Pro details below:" \
-            --textfield "Jamf Pro URL","required" : true \
-            --textfield "${username_label}",required : true \
-            --textfield "${password_label}","secure : true,required : true" \
+            --textfield "Jamf Pro URL, required" \
+            --textfield "${username_label}, required" \
+            --textfield "${password_label}, secure, required" \
             --icon "${icon}" \
             --alignment "left" \
             --small \
@@ -156,7 +156,6 @@ credential_prompt() {
             --titlefont "${title_font}" \
             --json
     )
-    if [[ $? == 0 ]]
     then
         jamf_pro_url=$(echo "${server_details}" | ${plutil_path} -extract "Jamf Pro URL" xml1 -o - - | xmllint --xpath "string(//string)" -)
         username=$(echo "${server_details}" | ${plutil_path} -extract "${username_label}" xml1 -o - - | xmllint --xpath "string(//string)" -)
@@ -188,12 +187,13 @@ invalid_credentials_prompt() {
 # Prompt to choose new or existing group
 group_option_prompt() {
     echo "$(date '+%Y-%m-%d %H:%M:%S') INFO: Prompting to choose between new group or existing group" >> "${log_file}"
-    group_options=$(
+    if ! group_options=$(
         "${dialog_path}" \
             --title "Framework Fixer" \
             --message "Would you like to create a Smart Computer Group to redeploy the Jamf Management Framework to?" \
             --radio "group_selection" \
-            --selecttitle "Please select an option",radio --selectvalues "I already have a Smart Computer Group, Please create a Smart Computer Group" \
+            --selecttitle "Please select an option",radio \
+            --selectvalues "I already have a Smart Computer Group, Please create a Smart Computer Group" \
             --icon "${icon}" \
             --alignment "left" \
             --button2 \
@@ -201,9 +201,8 @@ group_option_prompt() {
             --titlefont "${title_font}" \
             --small
     )
-    if [[ $? == 0 ]]
     then
-        group_selection=$(echo ${group_options} | awk -F '"' '{print $4}')
+        group_selection=$(echo "${group_options}" | awk -F '"' '{print $4}')
     else
         echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING: User Cancelled" >> "${log_file}"
         invalidate_token
@@ -259,7 +258,7 @@ group_name_prompt() {
 
     # Prompt user to select a Smart Computer Group from the list
     echo "$(date '+%Y-%m-%d %H:%M:%S') INFO: Prompting to choose a Smart Computer Group" >> "${log_file}"
-    group_name=$(
+    if ! group_name=$(
         "${dialog_path}" \
             --title "Framework Fixer" \
             --message "Choose a Smart Computer Group from the list:" \
@@ -275,8 +274,6 @@ group_name_prompt() {
             --button1text "Select" \
             --json
     )
-
-    if [[ $? == 0 ]]
     then
         group_name=$(echo "${group_name}" | ${plutil_path} -extract SelectedOption raw -o - - 2>/dev/null) 
         # Replace spaces with %20 for API call
@@ -305,11 +302,11 @@ group_exists() {
 
 # Prompt for number of days since last Inventory Update
 days_prompt() {
-    days=$(
+    if ! days=$(
         "${dialog_path}" \
             --title "Framework Fixer" \
             --message "OK, we will create a Smart Computer Group based on the number of days since the last Inventory Update. Please enter the number of days." \
-            --textfield "Number of days","regex=^[0-9]+$,regexerror=Input must be a number,required : true" \
+            --textfield "Number of days, regex=^[0-9]+$, regexerror=Input must be a number, required: true" \
             --icon "${icon}" \
             --alignment "left" \
             --small \
@@ -319,9 +316,8 @@ days_prompt() {
             --titlefont "${title_font}" \
             --json
     )
-    if [[ $? == 0 ]]
     then
-        days=$(echo $days | awk -F '"' '{print $4}')
+        days=$(echo "$days" | awk -F '"' '{print $4}')
         new_group_prompt
     else
         echo "$(date '+%Y-%m-%d %H:%M:%S') INFO: User clicked on 'Back'" >> "${log_file}"
@@ -332,11 +328,11 @@ days_prompt() {
 # Request the name of the Smart Computer Group to be created
 new_group_prompt() {
     echo "$(date '+%Y-%m-%d %H:%M:%S') INFO: Requesting the name for the new Smart Computer Group" >> "${log_file}"
-    group_name=$(
+    if ! group_name=$(
         "${dialog_path}" \
             --title "Framework Fixer" \
             --message "Please enter a name for the new Smart Computer Group." \
-            --textfield "Group Name","required" : true \
+            --textfield "Group Name, required" \
             --icon "${icon}" \
             --alignment "left" \
             --small \
@@ -345,7 +341,6 @@ new_group_prompt() {
             --titlefont "${title_font}" \
             --json
     )
-    if [[ $? == 0 ]]
     then
         group_name=$(echo "${group_name}" | ${plutil_path} -extract "Group Name" xml1 -o - - | xmllint --xpath "string(//string)" -)
         # Replace spaces with %20 for API call
@@ -383,7 +378,7 @@ new_group_prompt() {
 
 # Prompt to indicate there are no members in the Smart Computer Group
 no_members_prompt() {
-    "${dialog_path}" \
+    if ! "${dialog_path}" \
         --title "Framework Fixer" \
         --message "There are 0 members in this Smart Computer Group.  No action required." \
         --icon "${icon}" \
@@ -392,7 +387,6 @@ no_members_prompt() {
         --messagefont "${message_font}" \
         --titlefont "${title_font}" \
         --button1text "OK"
-    if [[ $? != 0 ]]
     then
         echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING: User Cancelled" >> "${log_file}"
         invalidate_token
@@ -403,7 +397,7 @@ no_members_prompt() {
 
 # Show number of devices in the Smart Computer Group and ask if we should remediate
 remediation_prompt() {
-    remediation_check=$(
+    if ! remediation_check=$(
         "${dialog_path}" \
             --title "Framework Fixer" \
             --message "There are ${member_count} members in the Smart Computer Group.  Would you like to redeploy the Jamf Management Framework on all computers in this group?" \
@@ -416,7 +410,6 @@ remediation_prompt() {
             --titlefont "${title_font}" \
             --json
     )
-    if [[ $? == 2 ]]
     then
         echo "$(date '+%Y-%m-%d %H:%M:%S') INFO: Remediation choice: yes" >> "${log_file}"
         remediation_check="Yes"
@@ -432,7 +425,7 @@ redeployment_prompt() {
     command_file="/Users/Shared/dialogIndeterminate.txt"
     : > "$command_file"
 
-    "${dialog_path}" \
+    if ! "${dialog_path}" \
         --title "JSS Framework Fixer" \
         --message "We're working on it! This can take a while depending on how many computers are in the Smart Computer Group" \
         --icon "${icon}" \
@@ -443,7 +436,6 @@ redeployment_prompt() {
         --button1text "Cancel" \
         --progress --indeterminate \
         --commandfile "${command_file}" &
-    if [[ $? != 0 ]]
     then
         echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING: User Cancelled" >> "${log_file}"
         invalidate_token
@@ -472,12 +464,12 @@ create_group() {
 EOM
 
 # Create the Smart Computer Group
-echo "$(date '+%Y-%m-%d %H:%M:%S') INFO: Creating group called ${group_name}" >> ""${log_file}""
+echo "$(date '+%Y-%m-%d %H:%M:%S') INFO: Creating group called ${group_name}" >> "${log_file}"
 
 group_creation=$(check_response -X 'POST' "${jamf_pro_url}/api/v2/computer-groups/smart-groups" -H "accept: application/json" -H "Authorization: Bearer ${bearer_token}" -H "Content-Type: application/json" -d "${json_payload}" -b "${ap_balance_id}")
 if [[ $group_creation =~ ^(200|201|202|204)$ ]]
 then
-    echo "$(date '+%Y-%m-%d %H:%M:%S') INFO: Group called ${group_name} successfully created" >> ""${log_file}""
+    echo "$(date '+%Y-%m-%d %H:%M:%S') INFO: Group called ${group_name} successfully created" >> "${log_file}"
 else
     echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR: Group creation failed" >> "${log_file}"
     error_prompt
@@ -487,9 +479,9 @@ fi
 }
 
 error_prompt() {
-    "${dialog_path}" \
+    if ! "${dialog_path}" \
         --title "Framework Fixer" \
-        --message "Oops! An error occurred. Please check "${log_file}" for more details" \
+        --message "Oops! An error occurred. Please check ${log_file} for more details" \
         --icon "${icon}" \
         --overlayicon "caution" \
         --alignment "left" \
@@ -497,7 +489,6 @@ error_prompt() {
         --messagefont "${message_font}" \
         --titlefont "${title_font}" \
         --button1text "OK"
-    if [[ $? == 0 ]]
     then
         invalidate_token
         exit 1
@@ -546,7 +537,7 @@ get_bearer_token() {
         bearer_token=$(echo "${response}" | ${plutil_path} -extract access_token raw -)
         token_expiration=$(echo "${response}" | ${plutil_path} -extract expires_in raw -)
         now_epoch_utc=$(date -j -f "%Y-%m-%dT%T" "$(date -u +"%Y-%m-%dT%T")" +"%s")
-        token_expiration_epoch=$((${now_epoch_utc} + ${token_expiration} - 1))
+        token_expiration_epoch=$((now_epoch_utc + token_expiration - 1))
         check_token_expiration_prompt
     fi
 
